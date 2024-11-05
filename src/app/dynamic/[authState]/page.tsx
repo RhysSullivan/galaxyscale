c54747564a90adc369a7811f20b336bba968dc89
Dominik Ferber <dominik.ferber@gmail.com>
2024-11-05 06:10:30 +0200

    add generateStaticParams

--- src/app/dynamic/[authState]/page.tsx
+++ src/app/dynamic/[authState]/page.tsx
@@ -1,9 +1,14 @@
 import { Homepage } from "@/components/homepage";
 
+export const dynamic = "error";
+export async function generateStaticParams(): Promise<{ authState: string }[]> {
+  return [{ authState: "cookie-present" }, { authState: "cookie-present" }];
+}
+
 export default async function Home({
   params,
 }: {
-  params: Promise<{ authState: "cookie-present" | "cookie-missing" }>;
+  params: Promise<{ authState: string }>;
 }) {
   const authState = await params.then((p) => p.authState);
   return <Homepage signedIn={authState === "cookie-present"} />;
